eb76e53e8fa1def137d5a3516f2a4cedd75bbe9d
zainabturkmen <zainabturkmen7@gmail.com>
2024-11-01 14:51:05 +0430

    nextjs

--- app/(dashboard)/auth/[[...sign-in]]/page.js
+++ app/(dashboard)/auth/[[...sign-in]]/page.js
@@ -1,8 +1,9 @@
 import React from "react";
 
 const SignInPage = ({ params }) => {
-  
-  return <div>SignInPage</div>;
+  console.log(params);
+
+  return <div className="text-7xl">SignInPage</div>;
 };
 
 export default SignInPage;
